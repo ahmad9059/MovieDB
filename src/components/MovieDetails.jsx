dab81a59f7d6634fcb932b3fe0af0644ec1cc977
ahmad9059 <ahmadhassan9059@gmail.com>
2025-07-30 11:15:17 +0500

    new modules added

--- src/components/MovieDetails.jsx
+++ src/components/MovieDetails.jsx
@@ -1,19 +1,54 @@
 import React, { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { asyncLoadMovie, removeMovie } from "../store/actions/movieActions";
-import { useParams } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
+import Loading from "./Loading";
 
 const MovieDetails = () => {
+  const navigate = useNavigate();
   const { id } = useParams();
   const dispatch = useDispatch();
-
+  const { info } = useSelector((state) => state.movie);
   useEffect(() => {
     dispatch(asyncLoadMovie(id));
     return () => {
       dispatch(removeMovie());
     };
   }, []);
-  return <div>MovieDetails</div>;
+
+  return info ? (
+    <div
+      style={{
+        background: `linear-gradient(rgba(0,0,0,0.2), rgba(0,0,0,0.5), rgba(0,0,0,0.8)),url(https://image.tmdb.org/t/p/original/${info.detail.backdrop_path})`,
+        backgroundPosition: "top 10%",
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+      }}
+      className="w-screen h-screen px-[10%] "
+    >
+      <nav className="h-[10vh] w-full text-zinc-100 flex items-center gap-10 text-xl ">
+        <Link
+          onClick={() => navigate(-1)}
+          className="hover:text-secondary cursor-pointer ri-arrow-left-line"
+        ></Link>
+
+        <a target="_blank" href={info.detail.homepage}>
+          <i className="ri-external-link-fill"></i>
+        </a>
+        <a target="_blank" href="">
+          <i className="ri-earth-fill"></i>
+        </a>
+        <a
+          target="_blank"
+          href={`https://www.imdb.com/title/${info.externalId.imdb_id}/`}
+        >
+          imdb
+        </a>
+      </nav>
+    </div>
+  ) : (
+    <Loading />
+  );
 };
 
 export default MovieDetails;
